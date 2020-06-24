f7f4773522fe8e5e593f629122a6f55f85a37425
hrituja17 <55551450+hrituja17@users.noreply.github.com>
2020-06-24 21:15:22 +0530

    Removed unnecessary code

--- foodpjt/js/index.js
+++ foodpjt/js/index.js
@@ -1,84 +1,6 @@
 const db = firebase.firestore();
 
 
-function getuser(){
-  document.getElementById("users").style.display = "none";
-  //document.getElementById("susers").style.display = "none";
-  var user=document.getElementById("uname").value;
-  console.log(user);
-  var susersList = document.getElementById("susers");
-  db.collection('customer_collection')
-  .get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      document.getElementById("susers").style.display = "block";
-      if(doc.data().name===user){
-        susersList.innerHTML += "<div class='card'><h6>"+doc.data().name+"</h6><p>Email : "+doc.data().email+"</p><p>Address : "+doc.data().address+
-        "<p><p>Contact Number : "+doc.data().phone+"</p></div>"
-      }
-    });
-});
-//document.getElementById("users").style.display = "block";
-}
-var usersList = document.getElementById("users");
-document.getElementById("susers").style.display = "none";
-document.getElementById("users").style.display = "block";
-db.collection('customer_collection').get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        usersList.innerHTML += "<div class='card'><h6>"+doc.data().name+"</h6><p>Email : "+doc.data().email+"</p><p>Address : "+doc.data().address+
-        "<p><p>Contact Number : "+doc.data().phone+"</p></div>"
-    });
-});
-
-/*function getaddr(id) {
-  console.log("executed");
-  return db.collection('tiffen_service_details/'+id+'/')
-  .once('value')
-    .then(function(bref) {
-      console.log(bref.data());
-       var add=bref.data().Address;
-       console.log(add);
-        return {add};
-});
-}*/
-function getvendor(){
-  document.getElementById("vendors").style.display = "none";
-  //document.getElementById("susers").style.display = "none";
-  document.getElementById("svendors").style.display = "block";
-  var user=document.getElementById("name").value;
-  var svendorsList = document.getElementById("svendors");
-  db.collection('vendor_collection/vendors/registered_vendors/')
-  .get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      if(doc.data().Name===user){
-        svendorsList.innerHTML +=  "<div class='card'><p>Name : "+doc.data().Name+"</p><p>Email : "
-      +doc.data().Email+"</p><p>Contact Number : "
-      +doc.data().Phone+"</p></div>"
-      }
-    });
-});
-//document.getElementById("users").style.display = "block";
-}
-
-
-var vendorsList = document.getElementById("vendors");
-document.getElementById("svendors").style.display = "none";
-document.getElementById("vendors").style.display = "block";
-db.collection('vendor_collection/vendors/registered_vendors/').get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      //console.log(doc.data());
-      //var addr=getaddr(doc.data().Email)
-      //console.log(addr);
-      vendorsList.innerHTML +=  "<div class='card'><p>Name : "+doc.data().Name+"</p><p>Email : "
-      +doc.data().Email+"</p><p>Contact Number : "
-      +doc.data().Phone+"</p></div>"
-    });
-    //console.log("completed");
-});
-
-
-
-
-
 function sliderUpdate() {  
     var imageNumber = document.getElementById("imageNumber").value;
     var image = document.getElementById("image").files[0];
@@ -238,24 +160,7 @@ db.collection("onboarding_images").get().then(function(querySnapshot) {
         document.getElementById("addOnboardingImage").appendChild(colDiv);
     });
 });
-var totalearning = 0;
-db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot) {
 
-   querySnapshot.forEach(function(doc) {
-       totalearning += doc.data().totalCost;
-   });
-   earning.innerHTML += "<h3>" + totalearning + "</h3>";
-});
-
-
-  var total =0;
-     db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot) {
-    
-        querySnapshot.forEach(function(doc) {
-            total += doc.data().totalCost;
-        });
-        earnings.innerHTML += "<h3>" + total + "</h3>";
-    });
 
 
 // where(firebase.firestore.FieldPath.documentId(), '==', 'saibhavadeesh@gmail.com')
--- foodpjt/js/stats.js
+++ foodpjt/js/stats.js
@@ -6,8 +6,14 @@ var usersCount = document.getElementById("users-count");
 var vendorsChart = document.getElementById("vendors-chart");
 var vendorsCount = document.getElementById("vendors-count");
 
+var earningsChart = document.getElementById("earnings-chart");
+var earningsCount = document.getElementById("earnings-count");
+ 
+
 var users = 0;
 var vendors = 0;
+var earnings=0;
+
 
 db.collection("customer_collection").get().then(function(querySnapshot){
     querySnapshot.forEach(function(doc){
@@ -108,6 +114,64 @@ db.collection("vendor_collection/vendors/registered_vendors").get().then(functio
                     ticks: {
                         beginAtZero: true,
                         stepSize: 2
+                    }
+                }]
+            }
+            
+        }
+    });
+});
+
+
+
+
+db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").get().then(function(querySnapshot){
+    querySnapshot.forEach(function(doc){
+        earnings += doc.data().totalCost;
+    });
+    earningsCount.innerHTML += "<h4><b>" + earnings + "</b></h4>";
+
+
+    
+    var data = [earnings];
+    var labels = ['JUN']
+    new Chart(earningsChart, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    backgroundColor: '#00bb00',
+                    borderColor: '#00bb00',
+                    data: data
+                }
+            ]
+        },
+        options: {
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            hover:{
+                mode: 'index',
+                intersect: true
+            },
+            scales:{
+                yAxes: [{
+                    gridLines: {
+                        display: true,
+                        lineWidth: '4px',
+                        color: 'rgba(0, 0, 0, 0.2)',
+                        zeroLineColor: 'transparent'
+                    },
+                    scaleLabel:{
+                        display: true,
+                        labelString: 'Total Earnings',
+                        fontColor: 'black',
+                    },
+                    ticks: {
+                        beginAtZero: false,
+                        stepSize: 5
                     }
                 }]
             }
